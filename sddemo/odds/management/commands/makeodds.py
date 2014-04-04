c9445b4a1f4b029aa16ce9133aee9d9423e055b4
Jonas Hagstedt <hagstedt@gmail.com>
2014-04-04 14:43:59 +0200

    updated management command to allow for random odds update

--- sddemo/odds/management/commands/makeodds.py
+++ sddemo/odds/management/commands/makeodds.py
@@ -5,19 +5,21 @@ import time
 from sddemo.odds.models import Odds
 
 
-def produce_odds():
+def produce_odds(random_odd):
     odds = Odds.objects.all()
+    if random_odd:
+        odds = [odds.order_by('?').first()]
     for o in odds:
         o.odds = decimal.Decimal(random.randrange(10000))/100
         o.save()
 
 
 class Command(BaseCommand):
-    def handle(self, frequency=1, **options):
+    def handle(self, frequency=1, random_odd=False, **options):
         frequency = decimal.Decimal(frequency)
         while(True):
             try:
-                produce_odds()
+                produce_odds(random_odd)
                 time.sleep(frequency)
             except:
                 return
